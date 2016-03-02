4adfca2c22949f03bc5307b59a7292e5c4f88b75
webdevmaurice <webdevmaurice@gmail.com>
2016-03-02 22:48:39 +0400

    New

---
+++ database/migrations/2016_01_30_195135_log.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Log extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('events');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('log');
+    }
+}
